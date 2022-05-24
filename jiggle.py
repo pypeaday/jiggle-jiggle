74ecf2f09e57650b0f40089d4d8fdbaa6b9a90e3
Nicholas Payne <nic.payne@protonmail.com>
2022-05-24 09:33:39 -0500

    remove rest of functionality except press

--- jiggle.py
+++ jiggle.py
@@ -17,45 +17,6 @@ def press(*args):
         win32api.keybd_event(VK_CODE[i], 0, win32con.KEYEVENTF_KEYUP, 0)
 
 
-def pressAndHold(*args):
-    """
-    press and hold. Do NOT release.
-    accepts as many arguments as you want.
-    e.g. pressAndHold('left_arrow', 'a','b').
-    """
-    for i in args:
-        win32api.keybd_event(VK_CODE[i], 0, 0, 0)
-        time.sleep(0.05)
-
-
-def pressHoldRelease(*args):
-    """
-    press and hold passed in strings. Once held, release
-    accepts as many arguments as you want.
-    e.g. pressAndHold('left_arrow', 'a','b').
-
-    this is useful for issuing shortcut command or shift commands.
-    e.g. pressHoldRelease('ctrl', 'alt', 'del'), pressHoldRelease('shift','a')
-    """
-    for i in args:
-        win32api.keybd_event(VK_CODE[i], 0, 0, 0)
-        time.sleep(0.05)
-
-    for i in args:
-        win32api.keybd_event(VK_CODE[i], 0, win32con.KEYEVENTF_KEYUP, 0)
-        time.sleep(0.1)
-
-
-def release(*args):
-    """
-    release depressed keys
-    accepts as many arguments as you want.
-    e.g. release('left_arrow', 'a','b').
-    """
-    for i in args:
-        win32api.keybd_event(VK_CODE[i], 0, win32con.KEYEVENTF_KEYUP, 0)
-
-
 if __name__ == "__main__":
     while True:
         press("F15")
